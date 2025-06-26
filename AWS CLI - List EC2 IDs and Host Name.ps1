5babf80dd4faa7f9cc8a490cd6ca4ef1680aacf5
Brogs <Kyle.M.Brogan@gmail.com>
2025-06-26 13:53:03 -0600

    Add files via upload

---
+++ AWS CLI - List EC2 IDs and Host Name.ps1
@@ -0,0 +1,13 @@
+﻿# EC2 ID only 
+aws ec2 describe-instances \
+  --query 'Reservations[].Instances[].InstanceId' \
+  --output text | tr '\t' '\n'
+
+
+# EC2 Name tag only, will print in order to match last step. 
+
+
+
+aws ec2 describe-instances \
+  --query "Reservations[].Instances[].Tags[?Key=='Name'].Value[]" \
+  --output text | tr '\t' '\n'
